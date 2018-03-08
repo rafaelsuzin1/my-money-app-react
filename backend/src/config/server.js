a646786b8038540528fe5c0d3086aeeea09ff0d4
suzin <rafael.suzin@itagtecnologia.com.br>
2018-03-08 19:47:02 -0300

    Foi concluído o backend da aplicação

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -3,10 +3,16 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const allowedCors = require('./cors')
+const queryParser = require('express-query-int')
 
-server.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowedCors)
+server.use(queryParser())
 
-server.listen(port, function(){
-    console.log(`BACKEND is running on port ${port}`)
+server.listen(port, function() {
+    console.log(`BACKEND is running on port ${port}.`)
 })
+
+module.exports = server
