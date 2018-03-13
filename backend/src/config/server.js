7bc12849cf53fcde2f524f4cb7b7386e8d7e6cd5
suzin <rafael.suzin@itagtecnologia.com.br>
2018-03-13 20:04:59 -0300

    Foram resolvidos os problemas com autenticação no backend

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -3,15 +3,15 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
-const allowedCors = require('./cors')
+const allowCors = require('./cors')
 const queryParser = require('express-query-int')
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
-server.use(allowedCors)
+server.use(allowCors)
 server.use(queryParser())
 
-server.listen(port, function() {
+server.listen(process.env.PORT || port, function() {
     console.log(`BACKEND is running on port ${port}.`)
 })
 
