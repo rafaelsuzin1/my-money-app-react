c4fe7e65330c3922dbf19402b8175dc5cddf8d83
suzin <rafael.suzin@itagtecnologia.com.br>
2018-03-13 19:43:09 -0300

    Foi concluída a aplicação sem a Autenticação

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,4 +1,4 @@
-module.exports = function(req, res, next){
+module.exports = function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, PATCH, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, WWW-Authenticate, Authorization, Origin, Version");
