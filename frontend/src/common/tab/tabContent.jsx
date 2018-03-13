edf95960e3ed6a3074e6d88f1884217a00852c8e
suzin <rafael.suzin@itagtecnologia.com.br>
2018-03-12 22:23:25 -0300

    Foi concluído o dashboard via API e inciado o desenvolvimento do BillingCycle

---
+++ frontend/src/common/tab/tabContent.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import {bindActionCreators} from 'redux'
+import {connect} from 'react-redux'
+
+class TabContent extends Component {
+    render() {
+        const seleted = this.props.tab.selected === this.props.id
+        return (
+            <div id={this.props.id} className={`tab-pane ${seleted ? 'active': ''}`}>
+                {this.props.children}
+            </div>
+        )
+    }
+}
+const mapStateToProps = state => ({tab: state.tab})
+export default connect(mapStateToProps)(TabContent)
--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import {Router, Route, Redirect, hashHistory} from 'react-router'
 
 import Dashboard from '../dashboard/dashboard'
+import DashboardNoRedux from '../dashboardNoRedux/dashboardNoRedux'
 import BillingCycle from '../billingCycle/billingCycle'
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path='/' component={Dashboard} />
+        <Route path='/' component={DashboardNoRedux} />
         <Route path='billingCycles' component={BillingCycle} />
         <Redirect from='*' to='/' />
     </Router>
